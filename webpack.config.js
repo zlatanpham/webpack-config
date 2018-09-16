30d5aacb10d74c461c83a523b4072a89b408dcc9
zlatan <zlatan.pham@gmail.com>
2018-09-16 21:41:52 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const merge = require("webpack-merge");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+
+const parts = require("./webpack.parts");
+
+const commonConfig = merge([
+  { plugins: [new HtmlWebpackPlugin({ title: "Webpack demo" })] }
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    host: process.env.HOST,
+    port: process.env.PORT
+  })
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
+};
