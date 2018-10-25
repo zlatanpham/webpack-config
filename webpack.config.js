38de12cd23d47bb1237a27713dc235be5ffa328b
zlatan <zlatan.pham@gmail.com>
2018-10-25 15:25:35 +0700

    add chalk

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,65 @@
-const merge = require("webpack-merge");
-const PATHS = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
-const parts = require("./webpack.parts");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
-const dotenv = require("dotenv");
+const merge = require('webpack-merge')
+const PATHS = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const parts = require('./webpack.parts')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
+const dotenv = require('dotenv')
+const chalk = require('chalk')
+const log = console.log
 
-dotenv.config();
+dotenv.config()
 
 const commonConfig = merge([
   {
     resolve: {
-      modules: [PATHS.resolve(__dirname, "./src"), "node_modules"],
-      extensions: [".js", ".jsx", ".json"],
+      modules: [PATHS.resolve(__dirname, './src'), 'node_modules'],
+      extensions: ['.js', '.jsx', '.json'],
       alias: {
-        "@": PATHS.resolve(__dirname, "./src")
-      }
+        '@': PATHS.resolve(__dirname, './src'),
+      },
     },
     plugins: [
       new CaseSensitivePathsPlugin(),
-      new CleanWebpackPlugin(["dist", "build"]),
+      new CleanWebpackPlugin(['dist', 'build']),
       new HtmlWebpackPlugin({
-        title: "Webpack demo",
-        template: "./src/index.html"
-      })
-    ]
+        title: 'Webpack demo',
+        template: './src/index.html',
+      }),
+    ],
   },
-  parts.loadJavascript({ include: PATHS.app, exclude: /node_modules/ })
-]);
+  parts.loadJavascript({ include: PATHS.app, exclude: /node_modules/ }),
+])
 
 const productionConfig = merge([
   parts.extractCSS({
-    use: ["css-loader"]
+    use: ['css-loader'],
   }),
   {
     optimization: {
       splitChunks: {
-        chunks: "initial"
-      }
-    }
-  }
-]);
+        chunks: 'initial',
+      },
+    },
+  },
+])
 
 const developmentConfig = merge([
   parts.devServer({
     host: process.env.HOST,
-    port: process.env.PORT
+    port: process.env.PORT,
   }),
-  parts.loadCSS()
-]);
+  parts.loadCSS(),
+])
 
 module.exports = mode => {
-  console.log("enviroment");
-  console.log(process.env.PORT);
-  console.log(process.env.TEST);
+  log(chalk.bgRed('Enviroment'))
+  log(chalk.bgBlue(chalk.red(process.env.PORT)))
 
-  if (mode === "production") {
-    return merge(commonConfig, productionConfig, { mode });
+  if (mode === 'production') {
+    return merge(commonConfig, productionConfig, { mode })
   }
 
-  return merge(commonConfig, developmentConfig, { mode });
-};
+  return merge(commonConfig, developmentConfig, { mode })
+}
