73826e5d40e91e097f8a2c7c2e2e78000428e8b0
zlatan <zlatan.pham@gmail.com>
2018-11-01 15:56:29 +0700

    test linting

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,13 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./style.scss";
+import React from "react"
+import styles from "./style.scss"
 
-const Container = props => {
-  return (
-    <div className={styles.container}>My Container this is the container</div>
-  );
-};
+const Container = () => (
+  <div className={styles.container}>My Container this is the container</div>
+)
 
-Container.propTypes = {};
+Container.propTypes = {}
 
-export default Container;
+export default Container
