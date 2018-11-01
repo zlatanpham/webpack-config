6f958ba6ffedac18d01d41d6341d96c40aaa730e
zlatan <zlatan.pham@gmail.com>
2018-11-01 17:18:47 +0700

    minify css output

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CleanWebpackPlugin = require("clean-webpack-plugin")
 const Autoprefixer = require("autoprefixer")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 
 module.exports = mode => {
   const devMode = mode !== "production"
@@ -11,6 +12,9 @@ module.exports = mode => {
     output: {
       filename: "[name].[contenthash].js",
       path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
     },
     module: {
       rules: [
