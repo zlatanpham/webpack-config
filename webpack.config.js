9ac6e8433b7dce5d09cc4b2134ca9d0fe141fe79
zlatan <zlatan.pham@gmail.com>
2018-11-01 16:55:17 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,48 @@
-const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
 
-module.exports = mode => {
-  return {
-    output: {
-      filename: "bundle.js",
-      filename: "[name].[contenthash].js",
-      path: path.resolve(__dirname, "dist")
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_module/,
-          use: { loader: "babel-loader" }
-        },
-        {
-          test: /\.scss$/,
-          exclude: /node_module/,
-          use: [
-            mode !== "production"
-              ? "style-loader"
-              : MiniCssExtractPlugin.loader,
-            "css-loader?modules&importLoaders=1&localIdentName=___[hash:base64:5]",
-            "sass-loader"
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(["dist"], { verbose: true, dry: false }),
-      new HtmlWebpackPlugin({
-        title: "Custom template",
-        // Load a custom template (lodash by default see the FAQ for details)
-        template: "./src/index.html"
-      }),
-      new MiniCssExtractPlugin({
-        // Options similar to the same options in webpackOptions.output
-        // both options are optional
-        filename: mode !== "production" ? "[name].css" : "[name].[hash].css",
-        chunkFilename: mode !== "production" ? "[id].css" : "[id].[hash].css"
-      })
-    ],
-    devServer: {
-      contentBase: path.join(__dirname, "dist"),
-      compress: true,
-      port: 9000
-    }
-  };
-};
+module.exports = mode => ({
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_module/,
+        use: { loader: "babel-loader" }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_module/,
+        use: [
+          mode !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader?modules&importLoaders=1&localIdentName=___[hash:base64:5]",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"], { verbose: true, dry: false }),
+    new HtmlWebpackPlugin({
+      title: "Custom template",
+      // Load a custom template (lodash by default see the FAQ for details)
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: mode !== "production" ? "[name].css" : "[name].[hash].css",
+      chunkFilename: mode !== "production" ? "[id].css" : "[id].[hash].css"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+})
