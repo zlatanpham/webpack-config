ddce527035899ff5c5587b61224c71c71d6c316c
zlatan <zlatan.pham@gmail.com>
2018-12-06 11:54:07 +0700

    add react

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,6 @@
-console.log('app');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const target = document.querySelector('#root');
+console.log(document.documentElement.querySelector('#root'));
+ReactDOM.render(<div>Hello</div>, target);
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,41 @@
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
 
-module.exports = env => ({
+module.exports = mode => ({
   entry: {
     app: ['./src/app/index.js'],
     iframe: ['./src/iframe/index.js'],
   },
   output: {
     filename: '[name].[hash].js',
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname + '/dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      inject: 'head',
+      inject: 'body',
       title: 'Multiple entries app',
       template: './src/app/index.html',
       chunks: ['app'],
       filename: 'index.html',
     }),
     new HtmlWebpackPlugin({
-      inject: 'head',
+      inject: 'body',
       template: './src/iframe/index.html',
       title: 'Iframe',
       chunks: ['iframe'],
